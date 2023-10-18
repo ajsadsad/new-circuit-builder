2b9adc5fee4170e6eef7af16886ecd6deac1af82
SohanKumar-0 <sohan.kumar29@outlook.com>
2023-10-19 07:06:00 +1100

    V1

--- src/components/CircuitBuilderUI/OptionsMenu.js
+++ src/components/CircuitBuilderUI/OptionsMenu.js
@@ -19,17 +19,22 @@ export default function OptionsMenu({ processCircuit, redo, undo, index, lastInd
 
     return (
         <div className={styles.OptionsMenu}>
-            <div class=" dropup d-flex justify-content-end" >
-                <button class="btn btn-dark rounded-0 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                    </svg>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-dark ">
-                    <li><a class="dropdown-item">Save</a></li>
-                    <li><a class="dropdown-item">Reset Grid</a></li>
-                </ul>
+            <div style={{ position: 'absolute', top: '83px', right: '10px'}}>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdownButton">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 14 14">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Save</Dropdown.Item>
+                        <Dropdown.Item href="#">Open</Dropdown.Item>
+                        <Dropdown.Item href="#">Compress</Dropdown.Item>
+                        <Dropdown.Item href="#">Reset Grid</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
+            
             {showCancel && <button onClick={handleCancel}> Cancel </button>}
             <button onClick={handleCreateQasmJson}> Create QASM JSON </button>
             <button onClick={redo} disabled={!canRedo}> redo </button>
