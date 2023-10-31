23552e17c83d6f3d3e232a91b38a9cf5f047d7d5
SohanKumar-0 <sohan.kumar29@outlook.com>
2023-10-31 20:52:43 +1100

    V1

--- src/components/CircuitBuilderUI/OptionsMenu.js
+++ src/components/CircuitBuilderUI/OptionsMenu.js
@@ -1,5 +1,4 @@
-import React from 'react'
-//import '../../CircuitBuilderPage/CircuitBuilder.module.scss';
+import React from 'react';
 import styles from "../css/OptionsMenu.module.css";
 import { Dropdown } from 'react-bootstrap';
 
@@ -9,22 +8,34 @@ export default function OptionsMenu({ processCircuit, redo, undo, index, lastInd
     const canRedo = index < lastIndex;
 
     return (
-        <div  className={styles.OptionsMenu}>
-            <div class=" dropup d-flex justify-content-end" >
-                <button class="btn btn-dark rounded-0 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                    </svg>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-dark ">
-                    <li><a class="dropdown-item">Save</a></li>
-                    <li><a class="dropdown-item" onClick={weakCompress}>Weak Compress</a></li>
-                    <li><a class="dropdown-item" onClick={strongCompress}>Strong Compress</a></li>
-                    <li><a class="dropdown-item" onClick={clearAllGates}>Clear All</a></li>
-                    <li><a class="dropdown-item" onClick={processCircuit}>Process Circuit</a></li>
-                    <li><a class="dropdown-item" onClick={undo} disabled = { !canUndo }>Undo</a></li>
-                    <li><a class="dropdown-item" onClick={redo} disabled = { !canRedo }>Redo</a></li>
-                </ul>
+        <div className={styles.OptionsMenu}>
+            <div className="d-flex justify-content-between">
+                <div className="dropup">
+                    <button className="btn btn-dark rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Settings / View 
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-dark">
+                        <li><a className="dropdown-item">All Gates</a></li>
+                        <li><a className="dropdown-item" onClick={weakCompress}>My Gates</a></li>
+                        <li><a className="dropdown-item" onClick={strongCompress}>Code</a></li>
+                    </ul>
+                </div>
+                <div className="dropup">
+                    <button className="btn btn-dark rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-dark">
+                        <li><a className="dropdown-item">Save</a></li>
+                        <li><a className="dropdown-item" onClick={weakCompress}>Weak Compress</a></li>
+                        <li><a className="dropdown-item" onClick={strongCompress}>Strong Compress</a></li>
+                        <li><a className="dropdown-item" onClick={clearAllGates}>Clear All</a></li>
+                        <li><a className="dropdown-item" onClick={processCircuit}>Process Circuit</a></li>
+                        <li><a className="dropdown-item" onClick={undo} disabled={!canUndo}>Undo</a></li>
+                        <li><a className="dropdown-item" onClick={redo} disabled={!canRedo}>Redo</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
